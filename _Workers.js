df69d450d8261d7c182c0f7a63ae4cead7fe563c
0x7 <127721218+0xMastermind@users.noreply.github.com>
2023-12-17 15:51:51 +0800

    Create _Workers.js

---
+++ _Workers.js
@@ -0,0 +1,56 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequestBody(request) {
+  if (request.method === "GET" || request.method === "HEAD") {
+    return null;
+  }
+  try {
+    return await request.text();
+  } catch (error) {
+    console.error("Error reading request body:", error);
+    return null;
+  }
+}
+
+async function handleRequest(request) {
+  let url = new URL(request.url);
+  url.hostname = 'hostloc.com';
+  url.protocol = 'https:';
+
+  let newRequestHeaders = new Headers(request.headers);
+  newRequestHeaders.set('Host', url.hostname);
+  newRequestHeaders.set('X-Real-IP', request.headers.get('cf-connecting-ip'));
+  newRequestHeaders.set('X-Forwarded-For', request.headers.get('cf-ipcountry'));
+  newRequestHeaders.set('REMOTE-HOST', request.headers.get('cf-connecting-ip'));
+  newRequestHeaders.set('Referer', url.toString());
+
+  let requestBody = await handleRequestBody(request);
+
+  let newRequest = new Request(url, {
+    method: request.method,
+    headers: newRequestHeaders,
+    body: requestBody
+  });
+
+  try {
+    let response = await fetch(newRequest);
+
+    if (response.headers.get('Content-Type')?.includes('text')) {
+      let text = await response.text();
+      text = text.replace(/hostloc.com/g, 'test.iwanttogohome.workers.dev');
+
+      return new Response(text, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers
+      });
+    }
+
+    return response;
+  } catch (error) {
+    console.error("Error fetching new request:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
